d4454557aac689575c1d439a9d1c27a86d2086b8
darko38 <daryakozina0308@yandex.ru>
2022-12-05 23:18:43 +0300

    ДЗ1

---
+++ lecture1.py
@@ -0,0 +1,68 @@
+value=None
+#print(type(value))
+
+a = 1
+b = 1.23
+#print(type(a))
+#print(type(b))
+value=12345
+#print(type(value))
+s='qwert' #вывод строки
+
+#print(a,'-',b,'-',s)
+#print('{1} - {2} - {0}'.format(a,b,s))
+#print(f'{a} - {b} - {s}')
+
+p=False
+#print(p)
+
+list=[12,2,3]
+col=['1', '2', '3']
+#print(list)
+#print(col)
+
+#print('Введите a')
+#a=float(input())
+#print('Введите b')
+#b=float(input())
+#print(a,' + ', b, ' = ', a+b)
+#print('{} {}'.format(a,b))
+#print(f'{a} {b}')
+
+# a = 3
+# a +=3
+# print(a)
+
+# func = 1
+# T = 4
+# x = 123
+# print(func<T>x)
+
+f = [ 1,2,3,4]
+# print(f)
+# print(2 in f)
+# print(not 2 in f)
+
+is_odd = f[0] % 2 == 0
+#print(is_odd)
+
+# a=int(input('a = '))
+# b=int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же МАША!')
+# elif username == 'Марина':
+#     print('Я так ждала Вас, Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ')
+# else:
+#     print('Привет, ', username)
+
+list = [1,2,3,4,5]
+for i in list:
+    print(i)
